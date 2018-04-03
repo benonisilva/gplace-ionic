610203ae6956fbdd2c6caf4737e28848b4e4f097
benoni <benoni.silva@gmail.com>
2018-04-03 12:28:11 -0300

    inicio de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController, App } from 'ionic-angular';
 // import { StatusBar } from '@ionic-native/status-bar';
 // import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -9,18 +9,11 @@ import { TestePage } from '../pages/teste/teste';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform,
-    //statusBar: StatusBar,
-    //splashScreen: SplashScreen
-  ) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      //statusBar.styleDefault();
-      //splashScreen.hide();
-    });
+  rootPage:any = TestePage;
+
+  constructor(menu: MenuController, app: App) {
+    menu.enable(true);
   }
 }
 
--- src/pages/teste/teste.ts
+++ src/pages/teste/teste.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
-import { ModalController , NavController, NavParams } from 'ionic-angular';
-import { PlaceModalPage } from '../place-detail/place-modal';
-
-/**
- * Generated class for the TestePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-teste',
   templateUrl: 'teste.html',
 })
 export class TestePage {
-
-  constructor(public navCtrl: NavController,
-    private modalCtrl: ModalController,
-    public navParams: NavParams) {
-  }
-  open() {
-    let modal = this.modalCtrl.create(PlaceModalPage);
-    modal.present();
+  rootPage = HomePage;
+  constructor(public navCtrl: NavController) {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad TestePage');
